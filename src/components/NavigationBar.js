de4760395194ffc10cd27f6c293637a2dca79968
Md. Raiyan Takrim <raiyan.takrim3@gmail.com>
2024-12-01 18:45:31 +0800

    Minor changes for the search bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -141,9 +141,11 @@ const SearchBar = ({ className }) => {
     return (
         <div className={cn("p-4 bg-[--link] ml-auto grid place-items-center", className)}>
             <Dialog>
-                <DialogTrigger><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg></DialogTrigger>
+                <DialogTrigger>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                </DialogTrigger>
                 <DialogContent>
                     <DialogHeader>
                         <DialogTitle>Type for search</DialogTitle>
@@ -156,7 +158,7 @@ const SearchBar = ({ className }) => {
                             />
                             <button
                                 type="submit"
-                                className="ml-2 p-2 bg-[--link] text-white rounded-md hover:bg-[--link-hover]"
+                                className="ml-2 p-2 bg-[--link] text-white rounded-md hover:bg-inherit hover:border hover:border-[--link] hover:text-[--link] focus:outline-none focus:ring-2 focus:ring-[--link]"
                             >
                                 Search
                             </button>
